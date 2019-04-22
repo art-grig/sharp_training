6fb4ad584cc0366e1df57003326363116a81fd4e
Artur Petrosyan <arm.arturpetrosyan@gmail.com>
2019-04-22 14:50:40 +0400

    Herti xndir@ lucaca

---
+++ ArturList/ArturList/ArtQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArturList
+{
+    public interface IQueue<T>
+    {
+        void Enqueue(T qElement);
+        T Dequeue();
+        int Count { get; }
+    }
+
+    public class ArtQueue<T> : IQueue<T>
+    {
+        private int _count;
+        private ArtNode<T> _current;
+        private ArtNode<T> _last;
+        public int Count { get { return _count; } }
+
+        public T Dequeue()
+        {
+            if (_count == 1) { return _last.Value; }
+            else
+            {
+                for (int i = 1; i < _count; i++)
+                {
+                    _current = _last.Prev;
+                    
+                }
+                return _current.Value;
+            }
+
+        }
+
+        public void Enqueue(T qElement)
+        {
+            _count++;
+            ArtNode < T > addNode = new ArtNode<T>();
+            if (_last == null)
+            {
+                _last = addNode;
+                _last.Value = qElement;
+
+                _current = addNode;
+                _current.Value = qElement;
+            }
+            else
+            {
+                addNode.Value = qElement;
+                _last.Prev = addNode;
+                _last = addNode;
+                    
+            }
+            
+            
+        }
+    }
+}
--- ArturList/ArturList/Program.cs
+++ ArturList/ArturList/Program.cs
@@ -1,77 +1,129 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ArturList
 {
+    public interface IArtList<T>
+    {
+        void Add(T value);
+        T Get(int hamar);
+    }
+
+    public class ArtNode<T>
+    {
+        public ArtNode<T> Next { get; set; }
+        public ArtNode<T> Prev { get; set; }
+        public T Value { get; set; }
+    }
+
+    public class ArtList<T> : IArtList<T>
+    {
+        private ArtNode<T> _last;
+        private ArtNode<T> _first;
+
+        public void Add(T value)
+        {
+            ArtNode<T> addNode = new ArtNode<T>();
+            addNode.Value = value;
+
+            if (_last == null)
+            {
+                _last = addNode;
+                _first = addNode;
+            }
+            else
+            {
+                _last.Next = addNode;
+                addNode.Prev = _last;
+                _last = addNode;
+            }
+
+        }
+
+        public T Get(int hamar)
+        {
+            ArtNode<T> currentElement;
+            currentElement = _first;
+
+            for ( int i = 1; i < hamar; i++)
+            {
+                currentElement = currentElement.Next;
+            }
+            return currentElement.Value;
+        }
+    }
+
+    public class ArtList { }
     class Program
     {
-		public interface IArtList
-		{
-			void Add(int value);
-			int Get(int hamar);
-		}
 
-		public class ArtNode
-		{
-			public ArtNode Next { get; set; }
-			public ArtNode Prev { get;  set; }
-			public int Value { get; set; }
-		}
+        private static void StugelListe()
+        {
+            ArtList<string> strList = new ArtList<string>();
+            strList.Add("a4aa");
+            strList.Add("a4aa");
+            strList.Add("aa5a");
+            strList.Add("aa5");
 
-		public class ArtList : IArtList
-		{
+            strList.Get(4);
+            ArtList<int> list = new ArtList<int>();
+            ArtList l = new ArtList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(10);
+            list.Add(20);
+
+            var ksan = list.Get(4);
+            var erku = list.Get(2);
+            var tas = list.Get(3);
+
+            if (ksan == 20 && erku == 2 && tas == 10)
+                Console.WriteLine("Cragire chisht ashxatec");
+            else
+                Console.WriteLine("Cragire normal chashxatec");
+        }
+
+        private static void StugelQueue()
+        {
             
-            private ArtNode _last ;
-            private ArtNode _first;
+            IQueue<int> queue = new ArtQueue<int>();
+            queue.Enqueue(3);
+            queue.Enqueue(1);
+            queue.Enqueue(6);
+            queue.Enqueue(10);
+
+            queue.Dequeue();
+            int ekrordElment = queue.Dequeue();
+            queue.Dequeue();
+
+            int esiminch = queue.Dequeue();
+
+            if (ekrordElment == 1 && queue.Count == 1)
+                Console.WriteLine("Queuen chisht ashxatec");
+            else
+                Console.WriteLine("Queuen sxal ashxatec");
+        }
+
+        static void StugelHivandanociHerte()
+        {
+            BjishkiHert hert = new BjishkiHert();
+
+            hert.KangnacnelHivandinHertiMech("Suro", "Gonzales", "Xelar");
+            hert.KangnacnelHivandinHertiMech("Karen", "xhazaryan", "poshkek");
+
+            Hivand endunvacHivand = hert.EndunelHivandin();
             
-			public void Add(int value)
-			{
-                ArtNode addNode = new ArtNode();
-                addNode.Value = value;
-                
-                if (_last == null )
-                {
-                    _last = addNode;
-                    _first = addNode;
-                }
-                else
-                {
-                   
-                   _last.Next = addNode;
-                   addNode.Prev = _last;
-                   _last = addNode;
-                }
-                
-			}
             
-			public int Get(int hamar)
-			{
-                ArtNode currentElement;
-                currentElement = _first;
-                
-                for (int i = 1; i < hamar; i++)
-                {
-                    currentElement = currentElement.Next;
-                }
-                return currentElement.Value;
-			}
-		}
+            if (endunvacHivand.Name == "Suro" && hert.Qanak == 1 )
+                Console.WriteLine("Cragire chisht ashxatec!");
+            else
+                Console.WriteLine("Cragire sxal ashxatec!");
+        }
 
 		static void Main(string[] args)
         {
-			ArtList list = new ArtList();
-			list.Add(1);
-			list.Add(2);
-			list.Add(10);
-			list.Add(20);
-            
-			var ksan = list.Get(4);
-			var erku = list.Get(2);
-			var tas = list.Get(3);
-            
-			if ( ksan == 20 && erku == 2 && tas == 10)
-				Console.WriteLine("Cragire chisht ashxatec");
-			else
-				Console.WriteLine("Cragire normal chashxatec");
+            // StugelListe();
+            //StugelQueue();
+            StugelHivandanociHerte();
         }
     }
 }
